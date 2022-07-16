af3f64d53e49c5fbcc580fc957b995b484912e4f
Илья <brandomanshop@gmail.com>
2022-07-16 22:32:17 +0300

    Улучшена производительность верстки

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
-import gulp from 'gulp';
-import del from 'del';
-import plumber from 'gulp-plumber';
-import htmlmin from 'gulp-htmlmin';
-import sass from 'gulp-dart-sass';
-import rename from 'gulp-rename';
-import svgo from 'gulp-svgmin';
-import svgstore from 'gulp-svgstore';
-import imagemin from 'gulp-imagemin';
-import webp from 'gulp-webp';
-import postcss from 'gulp-postcss';
-import csso from 'postcss-csso';
-import autoprefixer from 'autoprefixer';
-import browser from 'browser-sync';
+import gulp from "gulp";
+import del from "del";
+import plumber from "gulp-plumber";
+import htmlmin from "gulp-htmlmin";
+import sass from "gulp-dart-sass";
+import rename from "gulp-rename";
+import svgo from "gulp-svgmin";
+import svgstore from "gulp-svgstore";
+import imagemin from "gulp-imagemin";
+import webp from "gulp-webp";
+import postcss from "gulp-postcss";
+import csso from "postcss-csso";
+import terser from "gulp-terser";
+import autoprefixer from "autoprefixer";
+import browser from "browser-sync";
+import ghPages from "gulp-gh-pages";
 
+
+// Directory Clean
 
 const clean = () => {
   return del("build");
@@ -22,6 +26,8 @@ const clean = () => {
 
 const copy = () => {
   return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/*.ico",
     "source/img/**",
     "source/js/**",
     "source/*.ico"
@@ -53,21 +59,23 @@ const createWebp = () => {
 // Svg Sprite
 
 const sprite = () => {
-  return gulp.src('source/img/vector/sprite/*.svg')
+  return gulp.src("source/img/vector/sprite/*.svg")
     .pipe(svgstore({
       inlineSvg: true
     }))
-    .pipe(rename('sprite.svg'))
-    .pipe(gulp.dest('build/img/vector'));
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img/vector"));
 };
 
-const svgImages = () =>
-  gulp.src('source/img/*.svg')
-  .pipe(gulp.dest('build/img/vector'));
+// Svg Images
 
-const svgIcons = () => gulp.src('source/img/vector/*.svg')
+const svgImages = () =>
+  gulp.src("source/img/*.svg")
+  .pipe(gulp.dest("build/img/vector"));
+
+const svgIcons = () => gulp.src("source/img/vector/*.svg")
   .pipe(svgo())
-  .pipe(gulp.dest('build/img/vector'));
+  .pipe(gulp.dest("build/img/vector"));
 
 const reload = (done) => {
   browser.reload();
@@ -85,23 +93,37 @@ const html = () => {
 // CSS Styles
 
 const styles = () => {
-  return gulp.src('source/sass/style.scss', { sourcemaps: true })
+  return gulp.src("source/sass/style.scss", { sourcemaps: true })
     .pipe(plumber())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass().on("error", sass.logError))
     .pipe(postcss([
-      autoprefixer('last 2 versions'), csso()
+      autoprefixer("last 2 versions")
     ]))
-    .pipe(rename("style.min.css"))
-    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
+    .pipe(rename("styles.css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(postcss([
+      csso()
+    ]))
+    .pipe(rename("styles.min.css"))
+    .pipe(gulp.dest("build/css", { sourcemaps: "." }))
     .pipe(browser.stream());
+};
+
+// JS Minify
+
+const scripts = () => {
+  return gulp.src('source/js/*.js')
+  .pipe(terser())
+  .pipe(rename({suffix: ".min"}))
+  .pipe(gulp.dest("build/js"))
 };
 
 // Server
 
-const server = (done) => {
+export const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'build'
+      baseDir: "build"
     },
     cors: true,
     notify: false,
@@ -110,15 +132,23 @@ const server = (done) => {
   done();
 };
 
-
 // Watcher
 
 const watcher = () => {
-  gulp.watch('source/sass/**/*.scss', gulp.series(styles));
-  gulp.watch('source/*.html').on('change', browser.reload && gulp.series(html));
+  gulp.watch("source/sass/**/*.scss", gulp.series(styles));
+  gulp.watch("source/*.html").on("change", gulp.series(html, reload));
+  gulp.watch("source/js/*.js").on("change", gulp.series(scripts));
 };
 
+// Build
 
 export default gulp.series(
-  clean, copy, optimizeImages, createWebp, svgIcons, svgImages, sprite, reload, html, styles, server, watcher
+  clean, copy, optimizeImages, createWebp, svgIcons, svgImages, sprite, reload, html, styles, scripts, server, watcher
 );
+
+// Deploy GitHub Pages
+
+export const deploy = () => {
+  return gulp.src('./build/**/*')
+  .pipe(ghPages());
+}
